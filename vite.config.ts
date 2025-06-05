4f962e52af4e74e35d9b87255b9ed31c5768ebf2
mickael-o3o <mickael.00@outlook.com>
2025-06-05 16:03:23 +0300

    builded the project

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/mickael-o3o.github.io/recipe-page-solution/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
